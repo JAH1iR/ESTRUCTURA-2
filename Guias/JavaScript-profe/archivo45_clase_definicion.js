25ab308da91f3fd83b3fd50e2d6d2be16c6296df
Eric <bokanegrajahir@gmail.com>
2026-05-23 22:24:18 -0500

    se agregaron archivos del 22-5-2026 js-profe

---
+++ Guias/JavaScript-profe/archivo45_clase_definicion.js
@@ -0,0 +1,20 @@
+class Papel{
+    constructor(alto, ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
+
+let Papel_uno = class{
+    constructor(alto, ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
+
+let Papel_dos = class Papel_mio{
+    constructor(alto, ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
